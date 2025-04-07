e8be70524b16d6f7e3ff1784425b657ea52546a0
vicclu <ding.victor@gmail.com>
2025-04-07 19:07:21 +0200

    Vi har snakket med Hubert om mange af vores implementationer. Employer skulle vi slette, og vi har kun brug for ID for validering

--- src/main/java/app/Employee.java
+++
@@ -1,7 +0,0 @@
-package app;
-
-public class Employee extends User {
-    public Employee(String name, String email, String ID, String password){
-       super(name, email, ID, password);
-    }
-}
--- src/main/java/app/Employer.java
+++
@@ -1,7 +0,0 @@
-package app;
-
-public class Employer extends User{
-    public Employer(String name, String email, String ID, String password){
-        super(name, email, ID, password);
-    }
-}
