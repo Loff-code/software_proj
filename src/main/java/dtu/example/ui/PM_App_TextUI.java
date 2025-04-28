c5ed72d7d6bf4ef0c2bc773754747fe2523eda5d
loff-code <s235483@dtu.dk>
2025-04-28 21:30:58 +0200

    ui enum

--- src/main/java/dtu/example/ui/PM_App_TextUI.java
+++ src/main/java/dtu/example/ui/PM_App_TextUI.java
@@ -11,17 +11,24 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.util.Arrays;
 
-
 public class PM_App_TextUI implements PropertyChangeListener {
 	PM_App app;
 
 	String projectName;
 	String activityName;
+	String userID;
+
+	enum ProcessStep {
+		LOGIN, MAIN_MENU, SELECT_PROJECT, PROJECT_MENU, SELECT_ACTIVITY, ACTIVITY_MENU,
+		ASSIGN_USER, VIEW_ASSIGNED_USERS, ASSIGN_PROJECT_LEADER,
+		CREATE_ACTIVITY, CREATE_PROJECT, USERS_MENU, LIST_ALL_USERS, LIST_VACANT_USERS, CREATE_USER
+	}
+
+	ProcessStep processStep = ProcessStep.LOGIN;
 
 	public PM_App_TextUI() {
 		this.app = new PM_App();
 		this.app.addObserver(this);
-
 	}
 
 	public PM_App_TextUI(PM_App app) {
@@ -30,17 +37,9 @@ public class PM_App_TextUI implements PropertyChangeListener {
 	}
 
 	public static void main(String[] args) throws Exception {
-//		int bananas = 5;
-//		int apples = 5;
-//		if (args.length == 2) {
-//			bananas = Integer.valueOf(args[0]);
-//			apples = Integer.valueOf(args[1]);
-//		}
 		new PM_App_TextUI().mainLoop(System.in, System.out);
 	}
 
-	int processStep = 0;
-	int stringThing[] = {0,2,4};
 	public void mainLoop(InputStream in, PrintStream out) throws IOException, OperationNotAllowedException {
 		BufferedReader rs = new BufferedReader(new InputStreamReader(in));
 		String choice = null;
@@ -53,113 +52,106 @@ public class PM_App_TextUI implements PropertyChangeListener {
 			if (choice == null) {
 				break;
 			}
-			int number =  Arrays.asList(0, 9, 10, 14).contains(processStep) ? 1: Integer.valueOf(choice);
-			if (number == 0) {
-				break;
-			}
+			int number = Arrays.asList(ProcessStep.LOGIN, ProcessStep.CREATE_ACTIVITY, ProcessStep.CREATE_PROJECT, ProcessStep.CREATE_USER).contains(processStep)
+					? 1
+					: Integer.parseInt(choice);
 			processChoice(choice, number, out);
 
 		} while (choice != null);
 	}
-	String userID;
+
 	private void processChoice(String choice, int number, PrintStream out) throws OperationNotAllowedException {
-		int tmp = 0;
+		int tmp;
 		switch (processStep) {
-			case 0:
-				//LOGIN
+			case LOGIN:
 				app.login(choice);
 				userID = choice;
-				processStep ++;
-				app.createProject("test", "test");
-				app.getProjectByName("test").addActivity("test", 1, 1, 1);
-				app.createProject("test1", "test1");
-				app.getProjectByName("test1").addActivity("test1", 1, 1, 1);
+				processStep = ProcessStep.MAIN_MENU;
 				break;
-		case 1:
-			//MAIN MENU
-			switch (number) {
-				case 0:
-					app.logout();
-					processStep = 0;
-					break;
-				case 1:
-					processStep = 2;
-					break;
-				case 2:
-					processStep = 10;
-					break;
-				case 3:
-					processStep = 11;
-					break;
-				case 4:
-					processStep = 1;
-					break;
-			}
-			break;
-		case 2:
-			// SELECT PROJECT
-			tmp = 0;
-			for (Project project : app.getProject()) {
-				if (tmp == number) {
-					projectName = project.getName();
-					break;
+
+			case MAIN_MENU:
+				switch (number) {
+					case 0:
+						app.logout();
+						processStep = ProcessStep.LOGIN;
+						break;
+					case 1:
+						processStep = ProcessStep.SELECT_PROJECT;
+						break;
+					case 2:
+						processStep = ProcessStep.CREATE_PROJECT;
+						break;
+					case 3:
+						processStep = ProcessStep.USERS_MENU;
+						break;
+					case 4:
+						processStep = ProcessStep.MAIN_MENU;
+						break;
 				}
-				tmp++;
-			}
-			processStep = 3;
-			break;
-		case 3:
-			// PROJECT MENU
-			switch (number) {
-				case 0:
-					app.logout();
-					processStep = 0;
-					break;
-				case 1:
-					processStep = 4;
-					break;
-				case 2:
-					app.getProjectByName(projectName).addActivity("test", 1, 1, 1);
-					processStep = 9;
-					break;
-				case 3:
-					app.getProjectByName(projectName).setProjectLeader(app.getUserByID(app.getUserID()));
-					processStep = 8;
-					break;
-			}
-			break;
-		case 4:
-			// SELECT ACTIVITY
-			tmp = 0;
-			for (Activity activity : app.getProjectByName(projectName).getActivities()) {
-				if (tmp == number) {
-					activityName = activity.getName();
-					break;
+				break;
+
+			case SELECT_PROJECT:
+				tmp = 0;
+				for (Project project : app.getProject()) {
+					if (tmp == number) {
+						projectName = project.getName();
+						break;
+					}
+					tmp++;
 				}
-				tmp++;
-			}
-			processStep = 5;
-			break;
-		case 5:
-			// ACTIVITY MENU
-			switch (number) {
-				case 0:
-					app.logout();
-					processStep = 0;
-					break;
-				case 1:
-					processStep = 6;
-					break;
-				case 2:
-					processStep = 7;
-					break;
-				case 3:
-					processStep = 3;
-					break;
-			}
-			break;
-			case 6:
-				// ASSIGN USER
+				processStep = ProcessStep.PROJECT_MENU;
+				break;
+
+			case PROJECT_MENU:
+				switch (number) {
+					case 0:
+						app.logout();
+						processStep = ProcessStep.LOGIN;
+						break;
+					case 1:
+						processStep = ProcessStep.SELECT_ACTIVITY;
+						break;
+					case 2:
+						processStep = ProcessStep.CREATE_ACTIVITY;
+						break;
+					case 3:
+						app.getProjectByName(projectName).setProjectLeader(app.getUserByID(app.getUserID()));
+						processStep = ProcessStep.PROJECT_MENU;
+						break;
+				}
+				break;
+
+			case SELECT_ACTIVITY:
+				tmp = 0;
+				for (Activity activity : app.getProjectByName(projectName).getActivities()) {
+					if (tmp == number) {
+						activityName = activity.getName();
+						break;
+					}
+					tmp++;
+				}
+				processStep = ProcessStep.ACTIVITY_MENU;
+				break;
+
+			case ACTIVITY_MENU:
+				switch (number) {
+					case 0:
+						app.logout();
+						processStep = ProcessStep.LOGIN;
+						break;
+					case 1:
+						processStep = ProcessStep.ASSIGN_USER;
+						break;
+					case 2:
+						processStep = ProcessStep.VIEW_ASSIGNED_USERS;
+						break;
+					case 3:
+						processStep = ProcessStep.PROJECT_MENU;
+						break;
+				}
+				break;
+
+			case ASSIGN_USER:
 				tmp = 0;
 				for (User user : app.getUsers()) {
 					if (tmp == number) {
@@ -168,202 +160,169 @@ public class PM_App_TextUI implements PropertyChangeListener {
 					}
 					tmp++;
 				}
-				processStep = 5;
-				break;
-			case 7:
-				// GET ASSIGNED USERS
-				processStep = 5;
+				processStep = ProcessStep.ACTIVITY_MENU;
 				break;
 
-			case 8:
+			case VIEW_ASSIGNED_USERS:
+				processStep = ProcessStep.ACTIVITY_MENU;
+				break;
+
+			case ASSIGN_PROJECT_LEADER:
 				tmp = 0;
 				for (User user : app.getUsers()) {
 					if (tmp == number) {
-//						app.getProjectByName(projectName).getActivityByName(activityName).assignProjectLeader();
+						// no functionality here currently
 						break;
 					}
 					tmp++;
 				}
-				processStep = 5;
+				processStep = ProcessStep.PROJECT_MENU;
 				break;
-			case 9:
-				// CREATE ACTIVITY
+
+			case CREATE_ACTIVITY:
 				String[] parts = choice.split(" ");
 				if (parts.length == 4) {
-					app.getProjectByName(projectName).addActivity(parts[0], Integer.parseInt(parts[1]), Integer.parseInt(parts[2]), Integer.parseInt(parts[3]));
-
+					app.getProjectByName(projectName).addActivity(
+							parts[0],
+							Integer.parseInt(parts[1]),
+							Integer.parseInt(parts[2]),
+							Integer.parseInt(parts[3])
+					);
 				} else {
-					out.println("Invalid input");
+					out.println("Invalid input for creating activity");
 				}
-				processStep = 3;
+				processStep = ProcessStep.PROJECT_MENU;
 				break;
-			case 10:
-			// CREATE PROJECT
-			String[] parts0 = choice.split(" ");
-			if (parts0.length == 2) {
-				app.createProject(parts0[0], parts0[1]);
-			} else {
-				out.println("Invalid input");
-			}
-				processStep = 1;
-			break;
-			case 11:
-				// USER MENU
+
+			case CREATE_PROJECT:
+				String[] parts0 = choice.split(" ");
+				if (parts0.length == 2) {
+					app.createProject(parts0[0], parts0[1]);
+				} else {
+					out.println("Invalid input for creating project");
+				}
+				processStep = ProcessStep.MAIN_MENU;
+				break;
+
+			case USERS_MENU:
 				switch (number) {
 					case 0:
 						app.logout();
-						processStep = 0;
+						processStep = ProcessStep.LOGIN;
 						break;
 					case 1:
-						processStep = 12;
+						processStep = ProcessStep.LIST_ALL_USERS;
 						break;
 					case 2:
-						processStep = 13;
+						processStep = ProcessStep.LIST_VACANT_USERS;
 						break;
 					case 3:
-						processStep = 14;
+						processStep = ProcessStep.CREATE_USER;
 						break;
 					case 4:
-						processStep = 1;
+						processStep = ProcessStep.MAIN_MENU;
 						break;
 				}
 				break;
-				case 12:
-					// LIST ALL USERS
-					processStep = 11;
-					break;
-				case 13:
-					// LIST VACANT USERS
-					processStep = 11;
-					break;
-				case 14:
-					// CREATE USER
-					if (choice.length() < 5) {
-						app.createUser(choice);
-						processStep = 11;
-					} else {
-						out.println("Invalid input");
-					}
-					break;
-		default:
-			break;
+
+			case LIST_ALL_USERS:
+				processStep = ProcessStep.USERS_MENU;
+				break;
+
+			case LIST_VACANT_USERS:
+				processStep = ProcessStep.USERS_MENU;
+				break;
+
+			case CREATE_USER:
+				if (choice.length() <= 4) {
+					app.createUser(choice);
+				} else {
+					out.println("Invalid userID (max 4 characters)");
+				}
+				processStep = ProcessStep.USERS_MENU;
+				break;
 		}
 	}
 
-
-	private void showMenu(int processStep, PrintStream out) throws OperationNotAllowedException {
-		int tmp = 0;
-		switch (processStep) {
-			case 0:
-				// LOGIN
+	private void showMenu(ProcessStep step, PrintStream out) throws OperationNotAllowedException {
+		int tmp;
+		switch (step) {
+			case LOGIN:
 				out.println("Enter UserID: ");
 				break;
-			case 1:
-				// MAIN MENU
+			case MAIN_MENU:
 				out.println("UserID: " + userID);
 				out.println("   0) Logout");
 				out.println("   1) Select Projects");
 				out.println("   2) Create Project");
 				out.println("   3) Users");
 				out.println("   4) Log Time");
-				out.println("   5) Back");
-				out.println("Select a number (0-5): ");
+				out.println("Select a number (0-4): ");
 				break;
-			case 2:
-				// SELECT PROJECT
-				 tmp = 0;
+			case SELECT_PROJECT:
+				tmp = 0;
 				out.println("UserID: " + userID);
-				out.println("Select Project: ");
+				out.println("Select Project:");
 				for (Project project : app.getProject()) {
-					out.println("   " + tmp + " )  " + project.getName());
+					out.println("   " + tmp + ") " + project.getName());
 					tmp++;
 				}
 				break;
-			case 3:
-				// PROJECT
+			case PROJECT_MENU:
 				out.println("UserID: " + userID);
 				out.println("Project: " + projectName);
 				out.println("   0) Logout");
 				out.println("   1) Select Activity");
 				out.println("   2) Create Activity");
 				out.println("   3) Assign Project Leader");
-				out.println("   4) Back");
-				out.println("Select a number (0-4): ");
+				out.println("Select a number (0-3): ");
 				break;
-			case 4:
-				// SELECT ACTIVITY
+			case SELECT_ACTIVITY:
 				tmp = 0;
 				out.println("UserID: " + userID);
 				out.println("Project: " + projectName);
-				out.println("Select Activity: ");
+				out.println("Select Activity:");
 				for (Activity activity : app.getProjectByName(projectName).getActivities()) {
-					out.println("   " + tmp + " )  " + activity.getName());
+					out.println("   " + tmp + ") " + activity.getName());
 					tmp++;
 				}
 				break;
-			case 5:
-				// ACTIVITY
+			case ACTIVITY_MENU:
 				out.println("UserID: " + userID);
 				out.println("Project: " + projectName);
 				out.println("Activity: " + activityName);
-				out.println(app.getProjectByName(projectName).getActivityByName(activityName).getStartWeek() + " - " + app.getProjectByName(projectName).getActivityByName(activityName).getEndWeek());
 				out.println("   0) Logout");
 				out.println("   1) Assign User");
 				out.println("   2) View Assigned Users");
 				out.println("   3) Back");
 				out.println("Select a number (0-3): ");
 				break;
-			case 6:
-				// ASSIGN USER
+			case ASSIGN_USER:
 				tmp = 0;
 				out.println("UserID: " + userID);
-				out.println("Project: " + projectName);
-				out.println("Activity: " + activityName);
-				out.println(app.getProjectByName(projectName).getActivityByName(activityName).getStartWeek() + " - " + app.getProjectByName(projectName).getActivityByName(activityName).getEndWeek());
-				out.println("Select User: ");
+				out.println("Select User:");
 				for (User user : app.getUsers()) {
-					out.println("   " + tmp + " )  " + user.getID());
+					out.println("   " + tmp + ") " + user.getID());
 					tmp++;
 				}
 				break;
-			case 7:
-				// GET ASSIGNED USERS
+			case VIEW_ASSIGNED_USERS:
 				tmp = 0;
 				out.println("UserID: " + userID);
-				out.println("Project: " + projectName);
-				out.println("Activity: " + activityName);
-				out.println(app.getProjectByName(projectName).getActivityByName(activityName).getStartWeek() + " - " + app.getProjectByName(projectName).getActivityByName(activityName).getEndWeek());
-				for (String userID : app.getProjectByName(projectName).getActivityByName(activityName).getAssignedUsers()) {
-					out.println("   " + tmp + " )  " + userID);
+				for (String assigned : app.getProjectByName(projectName).getActivityByName(activityName).getAssignedUsers()) {
+					out.println("   " + tmp + ") " + assigned);
 					tmp++;
 				}
 				break;
-			case 8:
-				// ASSIGN PROJECT LEADER
-				tmp = 0;
-				out.println("UserID: " + userID);
-				out.println("Project: " + projectName);
-				out.println("Select User To Be Project Leader: ");
-				for (String userID : app.getProjectByName(projectName).getActivityByName(activityName).getAssignedUsers()) {
-					out.println("   " + tmp + " )  " + userID);
-					tmp++;
-				}
+			case CREATE_ACTIVITY:
+				out.println("Enter activity name, start week, end week and expected time: ");
+				out.println("Example: activityName 1 2 3");
 				break;
-			case 9:
-				//CREATE ACTIVITY
-				out.println("UserID: " + userID);
-				out.println("Project: " + projectName);
-				out.println("Write name, start week, end week and expected time: ");
-				out.println("Example: \"activity\" 1 2 3");
+			case CREATE_PROJECT:
+				out.println("Enter project name and client: ");
+				out.println("Example: projectName clientName");
 				break;
-			case 10:
-				//CREATE PROJECT
-				out.println("UserID: " + userID);
-				out.println("Write project name and client: ");
-				out.println("Example: \"project\" \"client\" ");
-				break;
-			case 11:
-				//USERS
+			case USERS_MENU:
 				out.println("UserID: " + userID);
 				out.println("   0) Logout");
 				out.println("   1) List All Users");
@@ -372,47 +331,30 @@ public class PM_App_TextUI implements PropertyChangeListener {
 				out.println("   4) Back");
 				out.println("Select a number (0-4): ");
 				break;
-			case 12:
-				//LIST ALL USERS
-				tmp=0;
-				out.println("UserID: " + userID);;
-				out.println("List Of All Users: ");
+			case LIST_ALL_USERS:
+				tmp = 0;
+				out.println("List of all users:");
 				for (User user : app.getUsers()) {
-					out.println("   " + tmp + " )  " + user.getID());
+					out.println("   " + tmp + ") " + user.getID());
 					tmp++;
 				}
 				break;
-			case 13:
-				//LIST VACANT USERS
-				tmp=0;
-				out.println("UserID: " + userID);;
-				out.println("List Of Vacant Users: ");
-				for (String userID : app.getProjectByName(projectName).getActivityByName(activityName).getAssignedUsers()) {
-					out.println("   " + tmp + " )  " + userID);
+			case LIST_VACANT_USERS:
+				tmp = 0;
+				out.println("Vacant users:");
+				for (String uid : app.getProjectByName(projectName).getActivityByName(activityName).getAssignedUsers()) {
+					out.println("   " + tmp + ") " + uid);
 					tmp++;
 				}
 				break;
-			case 14:
-				// CREATE USER
-				out.println("UserID: " + userID);;
-				out.println("Write userID (max 4 characters): ");
-				break;
-			default:
+			case CREATE_USER:
+				out.println("Enter userID (max 4 characters): ");
 				break;
 		}
-
-
-
 	}
-
-
-
-
-
 
 	@Override
 	public void propertyChange(PropertyChangeEvent evt) {
-
-
+		// Nothing here for now
 	}
 }
