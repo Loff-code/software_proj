ee99f9f60bb0f5be85f4d457e5108002e403c1e9
Alihamza2600 <s235479@dtu.dk>
2025-04-29 01:53:25 +0200

    aa

--- src/test/java/hellocucumber/AddProjectLeaderSteps.java
+++ src/test/java/hellocucumber/AddProjectLeaderSteps.java
@@ -2,55 +2,35 @@ package hellocucumber;
 
 import app.*;
 import io.cucumber.java.en.Given;
-//import io.cucumber.java.en.Then;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
-public class AddProjectLeaderSteps {
-
-    Employee tom = new Employee("Tom", "tom@world.com", "0004", "TomTom");
-    Employee jerry = new Employee("Jerry", "jerry@mail.com", "0003", "JerryJerry");
-    Employer carl = new EmployerHelper().getEmployer();
-
-    Project project;
-    private ErrorMessageHolder errorMessageHolder;
-    private PM_App app;
-
-    public AddProjectLeaderSteps(PM_App app, ErrorMessageHolder errorMessageHolder){
-        this.errorMessageHolder = errorMessageHolder;
-        this.app = app;
-    }
-
-    @Given("an employee is logged in")
-    public void an_employee_is_logged_in() throws OperationNotAllowedException {
-        try {
-            app.login(tom.getID(), tom.getPassword());
-        } catch (OperationNotAllowedException e) {
-            // Assert that the exception message is what you expect
-            assertEquals("Wrong password", e.getMessage());
-        }
-    }
-
-    @Given("that a project with ID {int} exists")
-    public void that_a_project_with_id_exists(int id) throws OperationNotAllowedException {
-        Client client = new ClientHelper().getClient();
-        project = new Project("Projectx", client);
 public class AddProjectLeaderSteps {
 
     Project project;
     private ErrorMessageHolder errorMessageHolder;
     String assignedUserID;
+    private PM_App app;
 
 
     public AddProjectLeaderSteps() {
         this.errorMessageHolder = new ErrorMessageHolder();
     }
 
-    // Create a new instance of PM_App with the given employees
-    PM_App app = AppHolder.app;
+
+
+
+
+
+
+
+
+
+
+
+
 
     // scenario 1: User assigns a project leader successfully
 
@@ -60,23 +40,6 @@ public class AddProjectLeaderSteps {
         project.assignProjectID(id);
         app.getProject().add(project);
 
-    }
-
-    @Given("that the employee {string} is not already a leader of the project")
-    public void that_the_employee_is_not_already_a_leader_of_the_project(String name) {
-        app.registerEmployee(jerry);
-        assertNull(project.getProjectLeaderID());
-    }
-
-    @When("Tom assigns {string} as the project leader to the project {int}")
-    public void tom_assigns_as_the_project_leader_to_the_project(String name, int id) throws OperationNotAllowedException {
-        app.assignProjectLeader(project.getName(), jerry.getID());
-
-    }
-
-    @Then("the employee {string} is set as the leader of project {int}")
-    public void the_project_leader_of_the_project_is(String name, int id) {
-        assertEquals(jerry.getID(), project.getProjectLeaderID());
     }
 
     @Given("the user {string} exists")
@@ -184,17 +147,18 @@ public class AddProjectLeaderSteps {
 
     //@When("the user {string} tries to assign themselves as project leader to project {int}")
     //public void the_user_tries_to_assign_themselves_as_project_leader_to_project(String name, int id) throws OperationNotAllowedException {
-      //  try {
-        //    app.assignProjectLeader(project.getProjectID(), assignedUserID);
-        //} catch (OperationNotAllowedException e) {
-         //   errorMessageHolder.setErrorMessage(e.getMessage());
-        //}
+    //  try {
+    //    app.assignProjectLeader(project.getProjectID(), assignedUserID);
+    //} catch (OperationNotAllowedException e) {
+    //   errorMessageHolder.setErrorMessage(e.getMessage());
+    //}
     //}
 
     //@Then("an error message happens about the user not being able to assign themselves as project leader")
     //public void an_error_message_happens_about_the_user_not_being_able_to_assign_themselves_as_project_leader() {
-      //  assertEquals("User cannot assign themselves as project leader", errorMessageHolder.getErrorMessage());
+    //  assertEquals("User cannot assign themselves as project leader", errorMessageHolder.getErrorMessage());
     //}
+
 
 
 }
