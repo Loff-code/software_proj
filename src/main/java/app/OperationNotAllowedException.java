e21a377f20815df64926f700ad00526e7050e2f6
loff-code <s235483@dtu.dk>
2025-05-05 18:45:39 +0200

    edit

--- src/main/java/app/OperationNotAllowedException.java
+++ src/main/java/app/OperationNotAllowedException.java
@@ -4,6 +4,6 @@ public class OperationNotAllowedException extends Exception {
     private static final long serialVersionUID = 5644804383994321392L;
 
     public OperationNotAllowedException(String errorMessage) {
-        super(errorMessage);
+        super(errorMessage);;
     }
 }
